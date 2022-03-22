6a2678e6d475395d765fa24b62059a456bfc5766
Aaron Meurer <asmeurer@gmail.com>
2022-03-21 18:15:34 -0600

    Update the update script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-git rm -rf docs/
-cp -R ../sympy/doc/_build/html docs/
+git rm -rf docs/dev/
+cp -R ../sympy/doc/_build/html docs/dev/
 touch docs/.nojekyll
 git add docs/
 git commit -m "Update the latest Furo theme"
